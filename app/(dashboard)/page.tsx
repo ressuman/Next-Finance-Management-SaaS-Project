0406f96c3b12556a2bf84ca7c668b0b99d072c7b
Richard Essuman <ressuman001@gmail.com>
2024-10-22 12:53:25 +0000

    "Added UserButton import and component to HomePage"

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
     <>
+      <UserButton />
       <Button variant={"destructive"}>Click me</Button>
       <p>This is an authenticated route</p>
     </>
