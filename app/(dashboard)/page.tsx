733293aaaa9cdbba61a0b6ec085099a27ac26e58
Richard Essuman <ressuman001@gmail.com>
2024-10-22 10:55:16 +0000

    "Removed page.tsx file and updated package-lock.json with new dependencies"

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function HomePage() {
+  return (
+    <>
+      <Button variant={"destructive"}>Click me</Button>
+      <p>This is an authenticated route</p>
+    </>
+  );
+}
