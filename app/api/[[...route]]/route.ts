b0b399cd6913cfc668795695e3c1d614ebbc7d7e
Richard Essuman <ressuman001@gmail.com>
2024-10-24 17:33:58 +0000

    "Added dependency 'hono' version 4.6.6 to package.json and updated package-lock.json accordingly."

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello Next.js!",
+    });
+  })
+  .get("hello/:test", (c) => {
+    const test = c.req.param("test");
+    return c.json({
+      message: "Hello Next.js! am back",
+      test,
+    });
+  });
+// .post("/hello", (c) => {
+//   return c.json({
+//     message: "Hello Next.js! am back",
+//   });
+// });
+
+export const GET = handle(app);
+export const POST = handle(app);
