9d49cb0f2e84155e9cf927dd75db3c776c700ad3
Richard Essuman <ressuman001@gmail.com>
2024-10-24 22:42:32 +0000

    "Added Zod validation to API routes using @hono/zod-validator"

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,9 +1,21 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
 import { handle } from "hono/vercel";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+
+const schema = z.object({
+  test: z.string(),
+});
+
+const schema1 = z.object({
+  test: z.string(),
+  age: z.number(),
+});
 
 app
   .get("/hello", (c) => {
@@ -11,18 +23,23 @@ app
       message: "Hello Next.js!",
     });
   })
-  .get("hello/:test", (c) => {
-    const test = c.req.param("test");
+  .get("hello/:test", zValidator("param", schema), (c) => {
+    const { test } = c.req.valid("param");
+
     return c.json({
       message: "Hello Next.js! am back",
       test,
     });
+  })
+  .post("/hi", zValidator("json", schema1), (c) => {
+    const { test, age } = c.req.valid("json");
+    return c.json({});
+    //   if (!result.success) {
+    //     return c.text("Invalid!", 400);
+    //   }
+    // })
+    //...
   });
-// .post("/hello", (c) => {
-//   return c.json({
-//     message: "Hello Next.js! am back",
-//   });
-// });
 
 export const GET = handle(app);
 export const POST = handle(app);
